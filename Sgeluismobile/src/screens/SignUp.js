ef62329045b6cd7571bc40808232e68f3b1b8c05
César López <cesarito1007.lopez@gmail.com>
2024-08-08 20:52:38 -0600

    en proceso de mejora del registro
    Registro en proceso, faltan algunas cosas que se mejoraran en la siguiente

--- Sgeluismobile/src/screens/SignUp.js
+++ Sgeluismobile/src/screens/SignUp.js
@@ -1,132 +1,44 @@
-
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import { useState } from 'react';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import * as Constantes from '../utils/constantes'
+import * as Constantes from '../utils/constantes';
 import Constants from 'expo-constants';
-//Import de componentes
-import Input from '../components/Inputs/Input'
-import InputMultiline from '../components/Inputs/InputMultiline'
-import Buttons from '../components/Buttons/Button';
-import MaskedInputTelefono from '../components/Inputs/MaskedInputTelefono';
-import MaskedInputDui from '../components/Inputs/MaskedInputDui';
+// Import de componentes
+import Input from '../components/Inputs/Input';
 import InputEmail from '../components/Inputs/InputEmail';
-
+import Buttons from '../components/Buttons/Button';
 
 export default function SignUp({ navigation }) {
     const ip = Constantes.IP;
 
-    const [date, setDate] = useState(new Date());
-    const [mode, setMode] = useState('date');
-    const [show, setShow] = useState(false);
-
-    const [nombre, setNombre] = useState('')
-    const [apellido, setApellido] = useState('')
-    const [email, setEmail] = useState('')
-    const [direccion, setDireccion] = useState('')
-    const [dui, setDui] = useState('')
-    const [telefono, setTelefono] = useState('')
-    const [fechaNacimiento, setFechaNacimiento] = useState('')
-    const [clave, setClave] = useState('')
-    const [confirmarClave, setConfirmarClave] = useState('')
-
-     // Expresiones regulares para validar DUI y teléfono
-     const duiRegex = /^\d{8}-\d$/;
-     const telefonoRegex = /^\d{4}-\d{4}$/;
-
-    /*
-    Codigo para mostrar el datetimepicker
-    */
-
-    const onChange = (event, selectedDate) => {
-        const currentDate = selectedDate;
-        setShow(false);
-        setDate(currentDate);
-        /*
-        Codigo para convertir la fecha al formato año-mes-dia */
-
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-        const day = String(currentDate.getDate()).padStart(2, '0');
-
-        const fechaNueva = `${year}-${month}-${day}`;
-        setFechaNacimiento(fechaNueva)
-    };
-
-    const showMode = (currentMode) => {
-        setShow(true);
-        setMode(currentMode);
-    };
-
-    const showDatepicker = () => {
-        showMode('date');
-    };
-
-    /*
-        Fin Codigo para mostrar el datetimepicker
-        */
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [alias, setAlias] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [clave, setClave] = useState('');
+    const [confirmarClave, setConfirmarClave] = useState('');
 
     const handleLogout = async () => {
-        /*
-                try {
-                    const response = await fetch(`${ip/EXPO2024/api/services/public/cliente.php?action=logOut`, {
-                        method: 'GET'
-                    });
-        
-                    const data = await response.json();
-        
-                    if (data.status) {
-                        navigation.navigate('Sesion');
-                    } else {
-                        console.log(data);
-                        // Alert the user about the error
-                        Alert.alert('Error', data.error);
-                    }
-                } catch (error) {
-                    console.error(error, "Error desde Catch");
-                    Alert.alert('Error', 'Ocurrió un error al iniciar sesión con bryancito');
-                } */
         navigation.navigate('Sesion');
     };
 
-    //props que recibe input
-    //placeHolder, setValor, contra, setTextChange
-
     const handleCreate = async () => {
         try {
-
-    // Calcular la fecha mínima permitida (18 años atrás desde la fecha actual)
-      const fechaMinima = new Date();
-      fechaMinima.setFullYear(fechaMinima.getFullYear() - 18);
             // Validar los campos
-            if (!nombre.trim() || !apellido.trim() || !email.trim() || !direccion.trim() ||
-                !dui.trim() || !fechaNacimiento.trim() || !telefono.trim() || !clave.trim() || !confirmarClave.trim()) {
+            if (!nombre.trim() || !apellido.trim() || !alias.trim() || !correo.trim() || !clave.trim() || !confirmarClave.trim()) {
                 Alert.alert("Debes llenar todos los campos");
                 return;
-            } else if (!duiRegex.test(dui)) {
-                Alert.alert("El DUI debe tener el formato correcto (########-#)");
-                return;
-            } else if (!telefonoRegex.test(telefono)) {
-                Alert.alert("El teléfono debe tener el formato correcto (####-####)");
-                return;
-            } else if (date > fechaMinima) {
-        Alert.alert('Error', 'Debes tener al menos 18 años para registrarte.');
-        return;
-      }
+            }
 
             // Si todos los campos son válidos, proceder con la creación del usuario
             const formData = new FormData();
-            formData.append('nombreCliente', nombre);
-            formData.append('apellidoCliente', apellido);
-            formData.append('correoCliente', email);
-            formData.append('direccionCliente', direccion);
-            formData.append('duiCliente', dui);
-            formData.append('nacimientoCliente', fechaNacimiento);
-            formData.append('telefonoCliente', telefono);
-            formData.append('claveCliente', clave);
+            formData.append('nombreProfesor', nombre);
+            formData.append('apellidoProfesor', apellido);
+            formData.append('aliasProfesor', alias);
+            formData.append('correoProfesor', correo);
+            formData.append('claveProfesor', clave);
             formData.append('confirmarClave', confirmarClave);
 
-            const response = await fetch(`${ip}/EXPO2024/api/services/public/profesores.php?action=signUpMovil`, {
+            const response = await fetch($,{ip}/EXPO2024/api/services/admin/profesores.php?action=signUp: {
                 method: 'POST',
                 body: formData
             });
@@ -143,8 +55,7 @@ export default function SignUp({ navigation }) {
         }
     };
 
-
-return (
+    return (
         <View style={styles.container}>
             <ScrollView contentContainerStyle={styles.scrollViewStyle}>
                 <Text style={styles.texto}>Registrar Usuario</Text>
@@ -160,48 +71,26 @@ return (
                 />
                 <InputEmail
                     placeHolder='Alias Profesor'
-                    setValor={email}
-                    setTextChange={setEmail} />
-                <InputMultiline
-                    placeHolder='Dirección Profesor'
-                    setValor={setDireccion}
-                    valor={direccion}
-                    setTextChange={setDireccion} />
-                <MaskedInputDui
-                    dui={dui}
-                    setDui={setDui} />
-                <View style={styles.contenedorFecha}>
-                    <Text style={styles.fecha}>Fecha Nacimiento</Text>
-
-                    <TouchableOpacity onPress={showDatepicker}><Text style={styles.fechaSeleccionar}>Seleccionar Fecha:</Text></TouchableOpacity>
-                    <Text style={styles.fecha}>Seleccion: {fechaNacimiento}</Text>
-
-                    {show && (
-         <DateTimePicker
-         testID="dateTimePicker"
-         value={date}
-         mode={mode}
-         is24Hour={true}
-         minimumDate={new Date(new Date().getFullYear() - 100, new Date().getMonth(), new Date().getDate())} // Fecha mínima permitida (100 años atrás desde la fecha actual)
-         maximumDate={new Date()} // Fecha máxima permitida (fecha actual)
-         onChange={onChange}
-       />
-                    )}
-                </View>
-
-                <MaskedInputTelefono
-                    telefono={telefono}
-                    setTelefono={setTelefono} />
+                    setValor={alias}
+                    setTextChange={setAlias}
+                />
+                <InputEmail
+                    placeHolder='Correo Profesor'
+                    setValor={correo}
+                    setTextChange={setCorreo}
+                />
                 <Input
                     placeHolder='Clave'
                     contra={true}
                     setValor={clave}
-                    setTextChange={setClave} />
+                    setTextChange={setClave}
+                />
                 <Input
                     placeHolder='Confirmar Clave'
                     contra={true}
                     setValor={confirmarClave}
-                    setTextChange={setConfirmarClave} />
+                    setTextChange={setConfirmarClave}
+                />
 
                 <Buttons
                     textoBoton='Registrar Usuario'
@@ -212,11 +101,8 @@ return (
                     textoBoton='Ir al Login'
                     accionBoton={handleLogout}
                 />
-
-
             </ScrollView>
         </View>
-
     );
 }
 
@@ -224,37 +110,23 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#778DA9',
-        paddingTop: Constants.statusBarHeight + 5, // el 5 es para darle un pequeño margen cuando hay una camara en el centro de la pantalla
-      },
+        paddingTop: Constants.statusBarHeight + 10, // Aumenta el margen superior
+    },
     scrollViewStyle: {
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        padding: 20, // Aumenta el padding alrededor del contenido
     },
     texto: {
-        color: '#322C2B', fontWeight: '900',
-        fontSize: 20
+        color: '#322C2B',
+        fontWeight: '900',
+        fontSize: 26, // Aumenta el tamaño de la fuente del título
+        marginBottom: 20, // Aumenta el espacio debajo del título
     },
     textRegistrar: {
-        color: '#322C2B', fontWeight: '700',
-        fontSize: 18
-    },
-
-    fecha: {
-        fontWeight: '600',
-        color: '#000000'
-    },
-    fechaSeleccionar: {
-        fontWeight: '700',
         color: '#322C2B',
-        textDecorationLine: 'underline'
-    },
-    contenedorFecha: {
-        backgroundColor: '#D9D9D9',
-        color: "#000000", fontWeight: '800',
-        width: 250,
-        borderRadius: 5,
-        padding: 5,
-        marginVertical: 10
+        fontWeight: '700',
+        fontSize: 24, // Aumenta el tamaño de la fuente de los textos de los botones
     }
 });
 
